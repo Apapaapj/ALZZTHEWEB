baab0b865d487a0204081e9a2445e636db5d1ae2
Alsyahadat <arkanjagoan@gmail.com>
2026-05-29 18:19:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function enterSite() {
+  const sound = document.getElementById("enterSound");
+
+  sound.currentTime = 0;
+  sound.play();
+
+  document.getElementById("entryScreen").style.display = "none";
+}
+
+function showPage(pageId){
+
+const sections = document.querySelectorAll('section');
+
+sections.forEach(section=>{
+section.classList.remove('active');
+});
+
+document.getElementById(pageId).classList.add('active');
+
+}
+
+function openPopup(){
+document.getElementById('popup').style.display='flex';
+}
+
+function closePopup(){
+document.getElementById('popup').style.display='none';
+}
